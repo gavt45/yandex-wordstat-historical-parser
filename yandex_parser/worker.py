0de2611b47a8b36731c77a3b6f407464da2e85a0
Andrey Gumirov <gavt45@bk.ru>
2022-11-24 01:49:10 +0700

    Added things

--- yandex_parser/worker.py
+++ yandex_parser/worker.py
@@ -1,2 +1,81 @@
-class Worker():
-    def __init__(self):
+import logging
+
+import redis
+import json
+
+
+class Worker:
+    TASKS_EXC = "tasks"
+    DONE_EXC = "done"
+
+    def __init__(self,
+                 host,
+                port,
+                 parser
+            ):
+        self.r = redis.Redis(host=host, port=port)
+        self.ps = self.r.pubsub()
+        self.ps.subscribe(self.TASKS_EXC)
+        self.parser = parser
+        self.done = False
+
+    def load(self, msg: str):
+        try:
+            m = json.loads(msg)
+            return m['uid'], m['kw'], m['cmd']
+        except json.decoder.JSONDecodeError:
+            return None, None, None
+        except KeyError:
+            return None, None, None
+
+    def dump(self, uid, res: dict):
+        return json.dumps(
+            {
+                "uid": uid,
+                "data": res
+             }
+        )
+
+    def work(self):
+        while not self.done:
+            dat = self.ps.get_message(timeout=1)
+            if not dat or dat['data'] == 1:
+                continue
+            try:
+                uid, kw, cmd = self.load(dat['data'])
+                logging.warn(f"[worker] Working: {uid} kw: {kw}")
+                if not uid:
+                    logging.warn(f"Strange msg: {dat}")
+                    continue
+
+                if cmd == 'exit':
+                    self.done = True
+                    self.parser.close()
+                    continue
+
+                data = self.parser.get(kw)
+
+                if not data:
+                    logging.warn("Return empty result!")
+                    data = {}
+                self.r.publish(self.DONE_EXC, self.dump(uid, data))
+            except Exception as e:
+                self.parser.close()
+                raise e
+            except KeyboardInterrupt as e:
+                self.parser.close()
+                self.done = True
+                raise e
+
+if __name__ == "__main__":
+    class ParserMock:
+        def get(self, kw):
+            return {"kw": kw}
+        def close(self):
+            logging.warn("parser EXIT!")
+    worker = Worker(
+        host="localhost",
+        port=6379,
+        parser=ParserMock()
+    )
+    worker.work()
